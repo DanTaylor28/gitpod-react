34333718d65b87e4b781d0b80a8220ad1feaa303
DanTaylor28 <dan.taylor1493@gmail.com>
2022-11-09 08:31:20 +0000

    Write my first functional greeting with props

--- my-app/src/components/FunctionalGreeting.js
+++ my-app/src/components/FunctionalGreeting.js
@@ -3,7 +3,7 @@ import React from 'react';
 // function FunctionalGreeting() {
 //     return <h1>Hello from React!</h1>
 // }
-
+// Same code as above just using arrow function
 const FunctionalGreeting = () => <h1>Hello From React!</h1>
 
 export default FunctionalGreeting;
