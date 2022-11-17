7e4b725a3195ba74b27b8f55a31e8cf3079dfbd7
DanTaylor28 <dan.taylor1493@gmail.com>
2022-11-17 09:22:45 +0000

    Create form where input field is controlled by react

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -15,11 +15,12 @@ import './App.css';
 // import RenderingLists from './components/RenderingLists';
 // import LifeCyclesCDM from './components/LifeCyclesCDM';
 // import LifeCyclesCDU from './components/LifeCyclesCDU';
-import LifeCyclesCWU from './components/LifeCyclesCWU';
+// import LifeCyclesCWU from './components/LifeCyclesCWU';
+import ControlledForms from './components/ControlledForms';
 function App() {
   return (
     <div className="App">
-      <LifeCyclesCWU />
+      <ControlledForms />
     </div>
   );
 }
