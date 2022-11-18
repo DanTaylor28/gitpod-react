90b412a25bc7f6ec852b83f15d2c4bd37376c7ce
DanTaylor28 <dan.taylor1493@gmail.com>
2022-11-18 14:32:00 +0000

    more learning about effects

--- my-app/src/components/UseEffectCounter.js
+++ my-app/src/components/UseEffectCounter.js
@@ -2,11 +2,24 @@ import React, {useState, useEffect} from 'react'
 
 function UseEffectCounter() {
     const [count, setCount] = useState(0);
+    const [time, setTime] = useState(0);
     useEffect(() => {
-        document.title = count
+        document.title = count;
     // second parameter [count] below, tells the useEffect function what properties to look for changes on before it executes. 
     // to stop it running after EVERY re-render which it would otherwise.
     }, [count]);
+    useEffect(() => {
+        console.log('creating timer');
+        const interval = setInterval(() => {
+            setTime(time => time + 1)
+        }, 1000);
+        // return statement below stops the timer incrementing by 2 instead of 1, this happens in development mode because react runs 
+        // in strict mode therfore certain functions/intervals run twice for some reason..
+        return () => {
+            clearInterval(interval);
+        };
+    // Passing an empty array below tells the useEffect function to run only once when the page firsts mounts/loads.
+    }, []);
   return (
     <div>
         <button onClick={
@@ -14,6 +27,7 @@ function UseEffectCounter() {
         }>
             Increment Count ({count})
         </button>
+        <h2>Time is {time}</h2>
     </div>
   )
 }
